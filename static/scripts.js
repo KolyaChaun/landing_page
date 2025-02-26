96c23bf1523fe200d2cd41d2dfdbb2274e20c23b
Mykola <kolia.chaun@icloud.com>
2025-02-26 22:46:35 +0200

    Sections about the course and about me have been implemented

--- static/scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const buttons = document.querySelectorAll(".cta-button, .price-box button");
-
-    buttons.forEach(button => {
-        button.addEventListener("click", () => {
-            alert("Запись временно недоступна. Свяжитесь с нами!");
-        });
-    });
-});
