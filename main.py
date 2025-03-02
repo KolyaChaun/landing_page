0d2088143f479ce935a599607e37368aa521ce94
Mykola <kolia.chaun@icloud.com>
2025-03-02 13:52:48 +0200

    A form for sending a request for consultation via telegram has been implemented.

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
+import requests
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from starlette.responses import JSONResponse, HTMLResponse
 
 app = FastAPI()
 
-# Подключаем папку со статическими файлами
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Подключаем папку с шаблонами
 templates = Jinja2Templates(directory="templates")
 
+BOT_TOKEN = '7208917368:AAGm6C4oKKDtpmY0JsNdDjLpJ7fuYxc3rDQ'
+CHAT_ID = '665565317'
 
-@app.get("/")
-def home(request: Request):
+def send_message_to_telegram(name: str, phone: str):
+    message = f"Новая заявка:\nИмя: {name}\nТелефон: {phone}"
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {
+        'chat_id': CHAT_ID,
+        'text': message
+    }
+    response = requests.post(url, data=payload)
+    return response
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+@app.post("/submit_form")
+async def submit_form(request: Request, name: str = Form(...), phone: str = Form(...)):
+    if name and phone:
+        send_message_to_telegram(name, phone)
+        return JSONResponse(content={"success": True})
+    return JSONResponse(content={"success": False})
 
-@app.post("/submit-form/")
-async def submit_form(name: str = Form(...), phone: str = Form(...)):
-    # Тут можно добавить обработку данных (например, сохранить в БД)
-    return {"message": f"Спасибо, {name}! Мы свяжемся с вами по номеру {phone}."}
+
+
