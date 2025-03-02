35ffffe3a7c95b32b330be971fcf279a63a81e24
Mykola <kolia.chaun@icloud.com>
2025-03-02 15:21:34 +0200

    Created gitignore and env file.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import os
+
 import requests
-from fastapi import FastAPI, Request, Form
-from fastapi.templating import Jinja2Templates
+from dotenv import load_dotenv
+from fastapi import FastAPI, Form, Request
 from fastapi.staticfiles import StaticFiles
-from starlette.responses import JSONResponse, HTMLResponse
+from fastapi.templating import Jinja2Templates
+from starlette.responses import HTMLResponse, JSONResponse
 
 app = FastAPI()
 
@@ -10,29 +13,27 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-BOT_TOKEN = '7208917368:AAGm6C4oKKDtpmY0JsNdDjLpJ7fuYxc3rDQ'
-CHAT_ID = '665565317'
+load_dotenv()
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
 
 def send_message_to_telegram(name: str, phone: str):
-    message = f"Новая заявка:\nИмя: {name}\nТелефон: {phone}"
+    message = f"Заявка на отримання косультації:\nІм'я: {name}\nТелефон: {phone}"
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    payload = {
-        'chat_id': CHAT_ID,
-        'text': message
-    }
+    payload = {"chat_id": CHAT_ID, "text": message}
     response = requests.post(url, data=payload)
     return response
+
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+
 @app.post("/submit_form")
-async def submit_form(request: Request, name: str = Form(...), phone: str = Form(...)):
+async def submit_form(name: str = Form(...), phone: str = Form(...)):
     if name and phone:
         send_message_to_telegram(name, phone)
         return JSONResponse(content={"success": True})
     return JSONResponse(content={"success": False})
-
-
-
