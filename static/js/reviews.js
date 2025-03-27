62c033b781255239f5aaa74f246df444a457117b
Mykola <kolia.chaun@icloud.com>
2025-03-28 00:06:31 +0200

    improved display of photo reviews in the mobile version

--- static/js/reviews.js
+++ static/js/reviews.js
@@ -2,11 +2,11 @@ document.addEventListener("DOMContentLoaded", function () {
     new Swiper(".swiper-container", {
         loop: true,
         centeredSlides: true,  // Центрирование активного слайда
-        slidesPerView: 3,      // Отображаем 3 слайда
-        spaceBetween: 15,      // Промежуток между слайдами
+        slidesPerView: 1,      // На мобильных устройствах отображаем только 1 слайд
+        spaceBetween: 0,       // Убираем промежуток между слайдами
         pagination: {
-            el: ".swiper-pagination",  // Активируем пагинацию
-            clickable: true            // Точки будут кликабельны
+            el: ".swiper-pagination",
+            clickable: true
         },
         navigation: {
             nextEl: ".next-btn",
@@ -14,12 +14,14 @@ document.addEventListener("DOMContentLoaded", function () {
         },
         breakpoints: {
             768: {
-                slidesPerView: 2,      // Для мобильных устройств будет отображаться 2 слайда
+                slidesPerView: 1,   // 1 слайд на мобильных
+                spaceBetween: 0,    // Без промежутков между слайдами
+                centeredSlides: true // Центрируем слайд на мобильных
             },
             1024: {
-                slidesPerView: 3,      // Для больших экранов 3 слайда
+                slidesPerView: 3,   // 3 слайда на экранах больше 1024px
+                spaceBetween: 15,    // Промежуток между слайдами
             }
         }
     });
 });
-
